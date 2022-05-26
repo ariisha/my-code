23248b5e70e454980709fa1528e0ced095198132
ariisha <106181637+ariisha@users.noreply.github.com>
2022-05-26 21:30:21 +0300

    Add files via upload

---
+++ DL/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Main {
+    public static void main (String[] args){
+        int ks;
+        Scanner num=new Scanner(System.in);
+        System.out.print("Input key length in bits:");
+        ks=num.nextInt();
+
+        BigInteger k2;
+        k2=new BigInteger("2");
+        BigInteger result= k2.pow(ks);
+        System.out.println("Keyspace:"+result);
+                     //Первое задание
+
+        final double max=0xffff;
+        final double rnd = rnd(max);
+
+    }
+}
